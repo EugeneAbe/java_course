6f3ade5f2ae96518479a053833c012cadcd6b7a5
EugeneAbe <pokjok7@mail.ru>
2020-02-09 11:46:20 +0300

    ДЗ2(4ый пункт)

--- sandbox/src/main/java/ru/ftry/homework2/CalcDistance.java
+++ sandbox/src/main/java/ru/ftry/homework2/CalcDistance.java
@@ -4,14 +4,11 @@ public class CalcDistance {
 
     public static void main(String[] args) {
 
-        Point p1 = new Point(11,22);
-        Point p2 = new Point(1, 2);
+        Point p1 = new Point(11,22, 1, 2);
 
-        System.out.println(distance(p1, p2));
+        System.out.println(p1.distance());
     }
 
 
-    public static double distance (Point p1, Point p2) {
-        return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
-    }
+
 }
--- sandbox/src/main/java/ru/ftry/homework2/Point.java
+++ sandbox/src/main/java/ru/ftry/homework2/Point.java
@@ -3,10 +3,17 @@ package ru.ftry.homework2;
 public class Point {
     public double x;
     public double y;
+    public double x2;
+    public double y2;
 
-    public Point (double x, double y) {
+    public Point (double x, double y, double x2, double y2) {
         this.x = x;
         this.y = y;
+        this.x2 = x2;
+        this.y2 = y2;
     }
 
+    public double distance () {
+        return Math.sqrt(((this.x2 - this.x) * (this.x2 - this.x)) + ((this.y2 - this.y) * (this.y2 - this.y)));
+    }
 }
