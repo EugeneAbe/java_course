8e0c450442bd9546486ee02184ff628d4b752d8b
EugeneAbe <pokjok7@mail.ru>
2020-02-09 11:33:21 +0300

    дз2(1-3 части)

---
+++ sandbox/src/main/java/ru/ftry/homework2/CalcDistance.java
@@ -0,0 +1,17 @@
+package ru.ftry.homework2;
+
+public class CalcDistance {
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point(11,22);
+        Point p2 = new Point(1, 2);
+
+        System.out.println(distance(p1, p2));
+    }
+
+
+    public static double distance (Point p1, Point p2) {
+        return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
+    }
+}
