5f143b6f5b346a7ab859db5958b1fdcfe992a563
EugeneAbe <pokjok7@mail.ru>
2020-02-15 18:13:20 +0300

    Рефакторинг теста на создание группы
    1) выделение вспомогательных методов;
    2) параметризация;
    3) Создание вспомогательных классов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.addressbook;
+
+public class GroupData {
+    private final String nameOfGroup;
+    private final String headerOfGroup;
+    private final String footerOfGroup;
+
+    public GroupData(String nameOfGroup, String HeaderOfGroup, String FooterOfGroup) {
+        this.nameOfGroup = nameOfGroup;
+        headerOfGroup = HeaderOfGroup;
+        footerOfGroup = FooterOfGroup;
+    }
+
+    public String getNameOfGroup() {
+        return nameOfGroup;
+    }
+
+    public String getHeaderOfGroup() {
+        return headerOfGroup;
+    }
+
+    public String getFooterOfGroup() {
+        return footerOfGroup;
+    }
+}
