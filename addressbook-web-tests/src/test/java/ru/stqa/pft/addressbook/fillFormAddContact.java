79f9538cf3a2b6c63d4e250a4f43c65f82f54889
EugeneAbe <pokjok7@mail.ru>
2020-02-15 19:16:14 +0300

    Homework_4
    1) Импортировал записанные рекордером тесты на добавление группы и добавление контаков;
    2) Отрефакторил их по трем описанным в лекции механизмам

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/fillFormAddContact.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class fillFormAddContact {
+    private final String contactFirstName;
+    private final String contactLastName;
+    private final String contactMidName;
+    private final String contactAddress;
+    private final String contactHomePhone;
+    private final String contactEmail;
+
+    public fillFormAddContact(String contactFirstName, String contactLastName, String contactMidName, String contactAddress, String contactHomePhone, String contactEmail) {
+        this.contactFirstName = contactFirstName;
+        this.contactLastName = contactLastName;
+        this.contactMidName = contactMidName;
+        this.contactAddress = contactAddress;
+        this.contactHomePhone = contactHomePhone;
+        this.contactEmail = contactEmail;
+    }
+
+    public String getContactFirstName() {
+        return contactFirstName;
+    }
+
+    public String getContactLastName() {
+        return contactLastName;
+    }
+
+    public String getContactMidName() {
+        return contactMidName;
+    }
+
+    public String getContactAddress() {
+        return contactAddress;
+    }
+
+    public String getContactHomePhone() {
+        return contactHomePhone;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+}
