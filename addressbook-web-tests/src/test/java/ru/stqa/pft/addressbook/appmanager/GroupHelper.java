e26bb0769186cbdefbd65b20786f47909c6f073c
EugeneAbe <pokjok7@mail.ru>
2020-02-16 18:29:06 +0300

    Добавлен класс BaseHelper
    В него вынесены однотипные методы, которые используются в нескольких других классах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -4,42 +4,35 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-public class GroupHelper {
-    private WebDriver wd;
+public class GroupHelper extends HelperBase  {
 
     public GroupHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
+        click(By.name("submit"));
     }
 
     public void fillGroupForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getNameOfGroup());
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeaderOfGroup());
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooterOfGroup());
+        type(By.name("group_name"), groupData.getNameOfGroup());
+        type(By.name("group_header"), groupData.getHeaderOfGroup());
+        type(By.name("group_footer"), groupData.getFooterOfGroup());
     }
 
     public void initGroupCreation() {
-        wd.findElement(By.name("new")).click();
+        click(By.name("new"));
     }
 
     public void returnToGroupPage() {
-      wd.findElement(By.linkText("group page")).click();
+        click(By.linkText("group page"));
     }
 
     public void deleteSelectedGroup() {
-      wd.findElement(By.name("delete")).click();
+        click(By.name("delete"));
     }
 
     public void selectGroup() {
-      wd.findElement(By.xpath("(//input[@name='selected[]'])[2]")).click();
+        click(By.xpath("(//input[@name='selected[]'])[2]"));
     }
 }
