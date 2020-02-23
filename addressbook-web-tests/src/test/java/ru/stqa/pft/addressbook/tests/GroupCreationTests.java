ffc52add7bd5493f3fc3ce408c01ee8a6a63ffe5
EugeneAbe <pokjok7@mail.ru>
2020-02-23 20:50:03 +0300

    промежуточное сохранение занятия 3
    1) в методе type реализовали проверку: если поле не нулл, то проверяем его значение и ничего не делаем, если оно эквивалентно передавамому параметру.
    2) Если передаваемый параметр равен нуллу, то ничего не далем с этм полем

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("Beatles", "Ringo Star", "Paul Maccartney"));
+        app.getGroupHelper().fillGroupForm(new GroupData("Beatles888", null, null));
         app.getGroupHelper().submitGroupCreation();
         app.logout();
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("Kino", "Zoyushka", "Ribin"));
+        app.getGroupHelper().fillGroupForm(new GroupData("Beatles", "Zoyushka", "Ribin"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
