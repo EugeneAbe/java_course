803e80b0210f38eabf845719bb605414535b3837
EugeneAbe <pokjok7@mail.ru>
2020-02-24 14:27:28 +0300

    оптимизирован переход между табами
    добавлена проверка, не находимся ли мы уже на той странице, на которую нам нужно перейти.
    Добавлено только для примеров из лекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -11,7 +11,14 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
-        click(By.linkText("groups"));
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        } else {
+            click(By.linkText("groups"));
+        }
+
     }
 
     public void gotoAddNewPage() {
@@ -19,6 +26,9 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoHome() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home"));
     }
 }
