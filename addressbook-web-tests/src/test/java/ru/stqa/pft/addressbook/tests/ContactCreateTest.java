d87597b6b3a43138236cbdceb4c60757d647aa28
EugeneAbe <60711867+EugeneAbe@users.noreply.github.com>
2020-03-11 10:53:37 +0300

    подправил тест для создания контаков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreateTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreateTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactCreateTest extends TestBase{
 
@@ -8,6 +9,10 @@ public class ContactCreateTest extends TestBase{
     @Test
     public void testContactCreate() throws Exception {
 
+        app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("Beatles", "RingoStar", "Paul Maccartney"));
+        }
         app.getNavigationHelper().gotoAddNewPage();
         app.getContactHelper().createContact(new ContactData("Jogan",
                 "Sebastjan", "Bach",
